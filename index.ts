382e0197d020f95c0cbf296c44ea18a58f85d5b0
Valdir Silva <valdir.ti@gmail.com>
2022-12-07 18:46:36 -0300

    chore: add vercel and convvert to typescript

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ const port = 8800
 const connect = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL || "");
         console.log('Connected to mongoDB')
     } catch (error) {
         throw error
