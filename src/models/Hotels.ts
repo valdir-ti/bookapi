ebca9ad99f6227c7ae373b2bcce83dc8b4252abd
Valdir Silva <valdir.ti@gmail.com>
2022-12-08 08:19:02 -0300

    feat: create hotel

---
+++ src/models/Hotels.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const hotelSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  type: {
+    type: String,
+    require: true,
+  },
+  city: {
+    type: String,
+    require: true,
+  },
+  address: {
+    type: String,
+    require: true,
+  },
+  distance: {
+    type: String,
+    require: true,
+  },
+  photos: {
+    type: [String],
+  },
+  title: { 
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  rooms: {
+    type: [String],
+    require: true,
+  },
+  cheapestPrice: {
+    type: Number,
+    require: true,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default model("Hotel", hotelSchema);
--- src/routes/hotels.ts
+++ src/routes/hotels.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import Hotels from "../models/Hotels";
 
 const hotelsRouter = express.Router();
 
 hotelsRouter.get("/", (req, res) => {
   res.json({ message: "this is the hotels endpoint" });
+});
+
+hotelsRouter.post("/", async (req, res) => {
+  const newHotel = new Hotels(req.body);
+
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 export { hotelsRouter };
